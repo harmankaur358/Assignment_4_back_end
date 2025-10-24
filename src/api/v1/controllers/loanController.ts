6d374ca0c8550fb821be1bd19cbe3b6bbfe42665
harman kaur <harmankaur2@academic.rrc.ca>
2025-10-24 12:51:18 -0500

    Added the controllers for loans.

--- src/api/v1/controllers/loanController.ts
+++ src/api/v1/controllers/loanController.ts
@@ -1,1 +1,29 @@
-fnff
+import { Request, Response } from "express";
+
+// create loan
+export const createLoan = (req: Request, res: Response) => {
+  res.status(201).json({
+    message: "Loan application is submitted successfully."
+  });
+};
+
+// get loans
+export const getLoans = (req: Request, res: Response) => {
+  res.json({
+    message: "List of all loan applications"
+  });
+};
+
+// review loan
+export const reviewLoan = (req: Request, res: Response) => {
+  res.json({
+    message: `Loan ${req.params.id} has been reviewed.`
+  });
+};
+
+// approve loan
+export const approveLoan = (req: Request, res: Response) => {
+  res.json({
+    message: `Loan ${req.params.id} has been approved.`
+  });
+};
